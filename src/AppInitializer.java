542c15334fbee99e4a76761efa1f34833ef05ce0
Supun Madubhashitha <you@example.com>
2023-04-03 02:15:10 +0530

    Create login form fxml file

---
+++ src/AppInitializer.java
@@ -0,0 +1,27 @@
+/*
+ * @author : Supun Jaysinghe
+ * Date    : 03 April 2023
+ * Time    : 2:01 AM
+ * Project : d24_hotel_manage_system
+ * Created by IntelliJ IDEA.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/lk/ccns/d24/view/LoginForm.fxml")))));
+        primaryStage.setTitle("D24 Hotel Manage System");
+        primaryStage.alwaysOnTopProperty();
+        primaryStage.show();
+    }
+}
---
+++ src/lk/ccns/d24/controller/LoginFormController.java
@@ -0,0 +1,33 @@
+/*
+ * @author : Supun Jaysinghe
+ * Date    : 03 April 2023
+ * Time    : 2:06 AM
+ * Project : d24_hotel_manage_system
+ * Created by IntelliJ IDEA.
+ */
+package lk.ccns.d24.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane loginFormAP;
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+
+    public void viewPasswordOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signupOnAction(ActionEvent actionEvent) {
+    }
+
+    public void forgotPasswordOnAction(MouseEvent mouseEvent) {
+
+    }
+}
