1c8c7f0e4bfcd519cdfda66e34b8d374e40f6b42
Supun Madubhashitha <you@example.com>
2023-04-14 23:17:22 +0530

    room manage case done

---
+++ src/lk/ccns/d24/bo/Custom/ManageRoomBO.java
@@ -0,0 +1,10 @@
+/*
+    * @author : Supun Jaysinghe
+    * Date    : 14 April 2023
+    * Time    : 11:01 PM
+    * Project : d24_hotel_manage_system
+    * Created by IntelliJ IDEA.
+*/
+
+package lk.ccns.d24.bo.Custom;public interface ManageRoomBO {
+}
---
+++ src/lk/ccns/d24/dao/custom/impl/RoomDAOImpl.java
@@ -0,0 +1,9 @@
+/*
+    * @author : Supun Jaysinghe
+    * Date    : 14 April 2023
+    * Time    : 11:15 PM
+    * Project : d24_hotel_manage_system
+    * Created by IntelliJ IDEA.
+*/
+package lk.ccns.d24.dao.custom.impl;public class RoomDAOImpl {
+}
