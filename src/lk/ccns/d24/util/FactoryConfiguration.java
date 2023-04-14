e87c98c2987a27c84c8fa01f48b0369ab3cf0861
Supun Madubhashitha <you@example.com>
2023-04-14 10:33:21 +0530

    create properti file configuration

---
+++ src/lk/ccns/d24/util/FactoryConfiguration.java
@@ -0,0 +1,9 @@
+/*
+    * @author : Supun Jaysinghe
+    * Date    : 14 April 2023
+    * Time    : 10:30 AM
+    * Project : d24_hotel_manage_system
+    * Created by IntelliJ IDEA.
+*/
+package lk.ccns.d24.util;public class FactoryConfiguration {
+}
