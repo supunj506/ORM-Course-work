9798ab363a917c2a0b31e7a37e039fe45af5aedd
Supun Madubhashitha <you@example.com>
2023-04-14 12:58:27 +0530

    create enntity class and configure

---
+++ src/lk/ccns/d24/entity/Reservation.java
@@ -0,0 +1,9 @@
+/*
+    * @author : Supun Jaysinghe
+    * Date    : 14 April 2023
+    * Time    : 12:40 PM
+    * Project : d24_hotel_manage_system
+    * Created by IntelliJ IDEA.
+*/
+package lk.ccns.d24.entity;public class Reservation {
+}
---
+++ src/lk/ccns/d24/entity/User.java
@@ -0,0 +1,9 @@
+/*
+    * @author : Supun Jaysinghe
+    * Date    : 14 April 2023
+    * Time    : 12:49 PM
+    * Project : d24_hotel_manage_system
+    * Created by IntelliJ IDEA.
+*/
+package lk.ccns.d24.entity;public class User {
+}
